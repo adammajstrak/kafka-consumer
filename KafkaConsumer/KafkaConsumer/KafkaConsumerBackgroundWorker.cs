355928a38b2444e86bb43a87315185424845473a
Majstrak, A. (Adam) <adam.majstrak@ing.com>
2023-12-23 14:50:47 +0100

    init

---
+++ KafkaConsumer/KafkaConsumer/KafkaConsumerBackgroundWorker.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaConsumer
+{
+    internal class KafkaConsumerBackgroundWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "MyGroupId",
+                BootstrapServers = "localhost:29092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("Test");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var result = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{result.Message.Value}' at: '{result.TopicPartitionOffset}'.");
+
+                            //Do whatever you want with your message
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ KafkaConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using KafkaConsumer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<KafkaConsumerBackgroundWorker>();
+using IHost host = builder.Build();
+
+await host.RunAsync();
